d9a3b101a47057b08187fd42ae81ca49bc508b3c
Andrew <andrewemoorehead@gmail.com>
2026-09-11 19:18:25 -0700

    small camera changes

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,6 +1,9 @@
 extends Camera3D
 
 @onready var base_offset: Vector3 = Vector3.UP*25
+var zoom: float = 0.0
+var wait: float = 0.4
+var bound: float = 20.0
 var offset:Vector3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	offset = base_offset + Vector3.DOWN*zoom + delta*%Player.velocity*wait
 	self.global_position = %Player.global_position+offset
+	self.global_position.x = clamp(self.global_position.x, bound*-1.0, bound)
+	self.global_position.y = clamp(self.global_position.y, bound*-1.0, bound)
 	
